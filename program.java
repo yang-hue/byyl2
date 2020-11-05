2c75491ff05e7ba2009842998bff0a7af5dc2f13
unknown <you@example.com>
2020-11-05 22:34:19 +0800

    b

--- program.java
+++ program.java
@@ -22,7 +22,7 @@ public class program {
 	        			if(topc=='#'||topc=='(') {
 	        				System.out.println("I+");
 	        				a[++top]='+';
-	        				if(in.charAt(i+1)=='\r') {
+	        				if(in.charAt(i+1)!='i'&&in.charAt(i+1)!='(') {
 	        					System.out.println("RE");
 	        					return;
 	        				}
@@ -73,7 +73,7 @@ public class program {
 	        			if(topc=='#'||topc=='('||topc=='+') {
 	        				System.out.println("I*");
 	        				a[++top]='*';
-	        				if(in.charAt(i+1)=='\r') {
+	        				if(in.charAt(i+1)!='i'&&in.charAt(i+1)!='(') {
 	        					System.out.println("RE");
 	        					return;
 	        				}
